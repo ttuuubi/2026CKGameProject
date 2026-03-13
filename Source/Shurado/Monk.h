5fad511ee7efc1028c7714042975c261497f0fa7
billra0423 <billra0423@gmail.com>
2026-03-13 11:02:51 +0900

    기본설정

---
+++ Source/Shurado/Monk.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PlayerCharacter.h"
+#include "Monk.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class SHURADO_API AMonk : public APlayerCharacter
+{
+	GENERATED_BODY()
+	
+};
